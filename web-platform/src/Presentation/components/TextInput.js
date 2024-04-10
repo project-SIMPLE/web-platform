473166fdf295d1be8c7eedfd701066de34103875
Jammel <sjammel1234@gmail.com>
2024-04-10 10:19:31 +0700

    Delete web-platform directory

--- web-platform/src/Presentation/components/TextInput.js
+++
@@ -1,9 +0,0 @@
-export default function TextInput(rest) {
-    return (
-        <input
-
-            style={{ height: 28, padding: 10, marginBottom: 10 }}
-            {...rest}
-        />
-    );
-}
